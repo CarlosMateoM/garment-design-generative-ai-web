99bc267dd684b0bbd3f4e7b3a378e998bcba1ffd
CarlosMateoM <carlosmateo484@gmail.com>
2024-12-06 17:14:39 -0500

    update: join websocket channel

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,23 @@
 <script setup>
    import { RouterView } from 'vue-router';
    import { useAuthStore } from '@/stores/auth';
-   import { onMounted } from 'vue';
+   import { onMounted, onUnmounted } from 'vue';
+   import echo from '@/echo';
+   import router from '@/router';
 
    const auth = useAuthStore();
 
    onMounted(async () => {
       await auth.getUser();
+      echo.private(`image-processed.${auth.user.id}`)
+         .listen('ImageProcessedEvent', async (e) => {
+            router.push({ name: 'design.show', params: { id: e.garmentDesign.id } });
+         });
+
+     
+   });
+
+   onUnmounted(() => {
+      echo.leaveChannel('image-processed');
    });
 </script>
--- src/views/design/DesignProcessing.vue
+++ src/views/design/DesignProcessing.vue
@@ -10,33 +10,34 @@
 
                 <main class="w-full">
 
-                    <transition enter-active-class="transition ease-out duration-300" enter-from-class="transform opacity-0 scale-95"
-        enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-200"
-        leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95"> 
-                    
-                    <section v-if="show" class="flex justify-center h-screen bg-slate-900 ">
-                        <div class="flex flex-col items-center  rounded m-2 p-4">
-                            <h2
-                                class="text-4xl font-semibold text-transparent bg-clip-text bg-gradient-to-r to-teal-200 from-lime-200 mb-16">
-                                Generando diseño
-                            </h2>
+                    <transition enter-active-class="transition ease-out duration-300"
+                        enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100"
+                        leave-active-class="transition ease-in duration-200"
+                        leave-from-class="transform opacity-100 scale-100"
+                        leave-to-class="transform opacity-0 scale-95">
 
+                        <section v-if="show" class="flex justify-center h-screen bg-slate-900 ">
+                            <div class="flex flex-col items-center  rounded m-2 p-4">
+                                <h2
+                                    class="text-4xl font-semibold text-transparent bg-clip-text bg-gradient-to-r to-teal-200 from-lime-200 mb-16">
+                                    Generando diseño
+                                </h2>
 
+                                <div class="relative">
 
-                            <div class="relative">
-
-                                <div
-                                    class="w-80 h-80 rounded-full animate-spin bg-gradient-to-r to-teal-200 from-lime-200">
-                                    <div class="absolute top-1 left-1 right-1 bottom-1 bg-slate-900/80 rounded-full">
+                                    <div
+                                        class="w-80 h-80 rounded-full animate-spin bg-gradient-to-r to-teal-200 from-lime-200">
+                                        <div
+                                            class="absolute top-1 left-1 right-1 bottom-1 bg-slate-900/80 rounded-full">
+                                        </div>
+                                    </div>
+                                    <div class="absolute inset-0 flex items-center justify-center">
+                                        <span class="text-white text-sm animate-pulse"> {{ text }}</span>
                                     </div>
                                 </div>
-                                <div class="absolute inset-0 flex items-center justify-center">
-                                    <span class="text-white text-sm animate-pulse"> {{ text }}</span>
-                                </div>
                             </div>
-                        </div>
-                    </section>
-                </transition>
+                        </section>
+                    </transition>
                 </main>
             </div>
         </template>
@@ -45,30 +46,16 @@
 <script setup>
     import PageLayaout from '@/layouts/PageLayaout.vue';
     import UserCard from '@/views/design/UserCard.vue';
-    import LoadingIcon from '@/components/icons/LoadingIcon.vue';
-    import { ref, onMounted, onUnmounted } from 'vue';
-    import { useAuthStore } from '@/stores/auth';
-    import echo from '@/echo';
-    import router from '@/router';
+    import { ref, onMounted } from 'vue';
 
     const text = ref('Por favor, espere...');
-
-    const auth = useAuthStore();
 
     const show = ref(false);
 
 
     onMounted(async () => {
-        echo.private(`image-processed.${auth.user.id}`)
-            .listen('ImageProcessedEvent', async (e) => {
-                router.push({ name: 'design.show', params: { id: e.garmentDesign.id } });
-            });
 
-            show.value = true;
-    });
-
-    onUnmounted(() => {
-        echo.leaveChannel('image-processed');
+        show.value = true;
     });
 
 </script>
